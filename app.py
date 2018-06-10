d778e103dc48e6e2661302c72f536341aae37f99
Дарья Пивоварова <dpivovarova@MacBook-Pro-Daria.local>
2018-06-10 21:40:52 +0300

    test change

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/timur')
+def timur():
+    return 'mur-mur-mur'
+
+@app.route('/dasha')
+def dasha():
+    return 'miau'
