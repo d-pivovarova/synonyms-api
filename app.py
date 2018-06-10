05df56ee10234340837c51def8c2b3de40c7966a
Дарья Пивоварова <dpivovarova@MacBook-Pro-Daria.local>
2018-06-10 21:50:48 +0300

    test change 2

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/timur')
-def timur():
-    return 'mur-mur-mur'
-
-@app.route('/dasha')
-def dasha():
-    return 'miau'
+@app.route('/synonym/<query>')
+def synonym(query):
+    return query
